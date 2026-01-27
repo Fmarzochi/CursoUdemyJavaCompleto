c9a99947f842504eeaf80d786cc0d42ad412c789
Felipe Marzochi <fmarzochi@gmail.com>
2026-01-27 10:52:45 -0300

    Sintaxes Opcionais

--- sintaxe_operacional/src/sintaxe_opcional.java
+++ sintaxe_operacional/src/sintaxe_opcional.java
@@ -2,7 +2,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
@@ -11,7 +11,6 @@ public class Main {
         int minutos = sc.nextInt();
 
         double conta = 50.0;
-
         if (minutos > 100) {
             conta += (minutos - 100) * 2.0;
         }
