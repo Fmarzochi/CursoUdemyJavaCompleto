88eb35a858d599d974d9861ad7d7c9df2918f075
Felipe Marzochi <fmarzochi@gmail.com>
2026-01-28 13:54:54 -0300

    Estruturas Repetitivas / (for)

---
+++ estruturas_repetitivas_for/src/contagem_regressiva.java
@@ -0,0 +1,9 @@
+public class contagem_regressiva {
+
+    public static void main(String[] args) {
+
+        for (int i=4; i>0; i--) {
+            System.out.println("Valor de i: " + i );
+        }
+    }
+}
